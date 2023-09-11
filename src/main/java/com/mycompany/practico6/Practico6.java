9d83a031376496075bd9d7d00be82dde76418042
SkinnyGuille <guillermo.n.rosales@gmail.com>
2023-09-10 22:46:13 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/practico6/Practico6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practico6;
+
+/**
+ *
+ * @author Familia
+ */
+public class Practico6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
